3e8066667cacbad40b6830f8dd4fe07c229a33c9
dxer <haifeng0730@126.com>
2015-05-27 16:07:29 +0800

    完善User-Agent

--- magic-digger/src/main/java/org/digger/fetch/Proxy.java
+++ magic-digger/src/main/java/org/digger/fetch/Proxy.java
@@ -1,15 +1,45 @@
 package org.digger.fetch;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
- * 
- * @class Proxy
  * @author linghf
  * @version 1.0
+ * @class Proxy
  * @since 2015年5月18日
  */
 public class Proxy {
+
+    /**
+     * User-Agent列表
+     */
+    private static List<String> userAgents = new ArrayList<String>();
+
+    static {
+        userAgents.add("Baiduspider");
+        userAgents.add("aibang.com");
+        userAgents.add("Googlebot");
+        userAgents.add("yahoo-slurp");
+        userAgents.add("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11");
+        userAgents.add("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER");
+        userAgents.add("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)");
+        userAgents.add("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400) ");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1");
+        userAgents.add("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E) ");
+        userAgents.add("Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0");
+        userAgents.add("Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0) ");
+    }
 
     public static String getRandomIP() {
         int number1 = new Random().nextInt(250) + 1;
@@ -18,21 +48,13 @@ public class Proxy {
         String ip = "183." + number1 + "." + number2 + "." + number3;
         return ip;
     }
-    
+
     /**
-     * 设置User-Agent
+     * 随机获得一个User-Agent
+     *
      * @return
      */
     public static String getRandomUa() {
-        String[] uas = {
-                        "Baiduspider",
-                        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
-                        "Dalvik/1.6.0 (Linux; U; Android 4.1.2; HS-E956Q Build/JZO54K)",
-                        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_6 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8E200 Safari/6533.18.5 RPT-HTTPClient/0.3-3",
-                        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E) ",
-                        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; 360space)",
-                        "aibang.com", "Googlebot", "yahoo-slurp",
-                        "Dalvik/1.6.0 (Linux; U; Android 4.1.2; HUAWEI A199 Build/HuaweiA199)"};
-        return uas[new Random().nextInt(10)];
+        return userAgents.get(new Random().nextInt(userAgents.size()));
     }
 }
