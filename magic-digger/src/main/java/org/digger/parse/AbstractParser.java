6d79b487178a4cc85b873664a9df4c02aaead879
dxer <haifeng0730@126.com>
2015-05-29 11:36:07 +0800

    完善代码，去掉删除的类

--- magic-digger/src/main/java/org/digger/parse/AbstractParser.java
+++ magic-digger/src/main/java/org/digger/parse/AbstractParser.java
@@ -1,16 +1,17 @@
 package org.digger.parse;
 
 import org.digger.manager.DiggerManager;
+import org.digger.model.WebPage;
 
 /**
  * Created by linghf on 2015/5/29.
  */
-public class AbstractParser implements Parser, Runnable {
+public abstract class AbstractParser implements Parser, Runnable {
 
     @Override
     public void run() {
         while (DiggerManager.isIsParserRun()) {
-
+            this.process();
         }
     }
 }
